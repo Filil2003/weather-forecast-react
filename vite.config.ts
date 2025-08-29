151bf45cf703a78e19ffd069326986b74b71b52e
Ilya <filil2003@yandex.ru>
2025-08-29 22:49:34 +0300

    chore: add vite-plugin-checker for dev-time checks

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      typescript: { tsconfigPath: "./tsconfig.app.json" },
+    }),
+  ],
 });
