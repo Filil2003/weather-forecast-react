2648d7a4c0385767e885e7216eff5e001a5d4b8d
Ilya <filil2003@yandex.ru>
2025-08-30 12:35:20 +0300

    chore: integrate vite-css-modules plugin
    - Enable CSS Modules support in TypeScript
    - Generate type declarations for .module.css files

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { patchCssModules } from "vite-css-modules";
 import checker from "vite-plugin-checker";
 
 export default defineConfig({
   plugins: [
     react(),
+    patchCssModules({
+      exportMode: "default",
+      generateSourceTypes: true,
+    }),
     checker({
       typescript: { tsconfigPath: "./tsconfig.app.json" },
     }),
