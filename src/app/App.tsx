a824c39a34c285d44e1620c80121584f74d0c791
Ilya <filil2003@yandex.ru>
2025-09-28 14:43:53 +0300

    feat(ui): add WeeklyForecast component (layout only)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,14 @@
 import "./styles/index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { WeeklyForecast } from "#entities/weather";
 
 const queryClient = new QueryClient();
 
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <h1>Weather Forecast</h1>
+      <WeeklyForecast />
       <ReactQueryDevtools />
     </QueryClientProvider>
   );
---
+++ src/entities/weather/ui/WeeklyForecast/WeeklyForecast.tsx
@@ -0,0 +1,41 @@
+import clsx from "clsx";
+import { Section } from "#shared/ui/Section";
+import { Text } from "#shared/ui/Text";
+import { WeatherIcon } from "../WeatherIcon/WeatherIcon.tsx";
+import { mockWeeklyForecast } from "./mock";
+import styles from "./WeeklyForecast.module.css";
+
+export function WeeklyForecast() {
+  return (
+    <Section heading="Weekly forecast" headingLevel="h2">
+      <ul className={styles.list}>
+        {mockWeeklyForecast.map((day, index) => (
+          <li
+            className={clsx(styles.item, { [styles.weekend]: index % 2 === 0 })}
+            key={day.date}
+          >
+            <div>
+              <Text as="p" className={styles.date} variant="caption">
+                {day.date}
+              </Text>
+              <Text as="p" className={styles.day} weight="bold">
+                {day.day}
+              </Text>
+            </div>
+            <WeatherIcon
+              code={day.code}
+              isDay={day.isDay}
+              size={"2.25em"}
+              title={day.condition}
+            />
+            <Text className={styles.condition}>{day.condition}</Text>
+            <Text as="p" className={styles.temperature}>
+              <Text weight="bold">{day.tempMax}</Text>
+              {day.tempMin}
+            </Text>
+          </li>
+        ))}
+      </ul>
+    </Section>
+  );
+}
