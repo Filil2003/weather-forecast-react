a81db272b5e5485d8b2c3c07e465a7ca3dfd07d5
Ilya <filil2003@yandex.ru>
2025-09-30 15:15:39 +0300

    feat(ui): add base Icon component

---
+++ src/shared/ui/Icon/ui/Icon.tsx
@@ -0,0 +1,56 @@
+import type { ComponentProps } from "react";
+
+type SpriteIcons = {
+  common:
+    | "ArrowDown"
+    | "ArrowLeft"
+    | "ArrowRight"
+    | "Cross"
+    | "Settings"
+    | "History"
+    | "Humidity"
+    | "Location"
+    | "Precipitation"
+    | "Pressure"
+    | "Raindrop"
+    | "Search"
+    | "Snowflake"
+    | "SnowWithRain"
+    | "UVIndex"
+    | "Visibility"
+    | "Wind";
+  flags: "RU" | "US";
+};
+
+type Sprites = keyof SpriteIcons;
+
+interface Props<T extends Sprites> extends ComponentProps<"svg"> {
+  title: string;
+  sprite: T;
+  name: SpriteIcons[T];
+  size?: number | string;
+}
+
+export function IconComponent<T extends Sprites>({
+  title,
+  sprite,
+  name,
+  size = 24,
+  ...restProps
+}: Props<T>) {
+  return (
+    <svg height={size} width={size} {...restProps}>
+      <title>{title}</title>
+      <use href={`icons/${sprite}.svg#${name}`} />
+    </svg>
+  );
+}
+
+export const Icon = Object.assign(IconComponent, {
+  Common: (props: Omit<Props<"common">, "sprite">) => (
+    <IconComponent sprite="common" {...props} />
+  ),
+  Flags: (props: Omit<Props<"flags">, "sprite">) => (
+    <IconComponent sprite="flags" {...props} />
+  ),
+});
