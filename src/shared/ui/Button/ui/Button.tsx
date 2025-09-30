7966a11e2926b88ea26b1dab08cbef87bc7b6b6d
Ilya <filil2003@yandex.ru>
2025-09-30 17:11:49 +0300

    feat(ui): add Button component

---
+++ src/shared/ui/Button/ui/Button.tsx
@@ -0,0 +1,22 @@
+import clsx from "clsx";
+import type { ComponentProps } from "react";
+import styles from "./Button.module.css";
+
+interface Props extends ComponentProps<"button"> {
+  shape?: "round" | "normal";
+}
+
+export function Button({
+  children,
+  shape = "normal",
+  type = "button",
+  ...restProps
+}: Props) {
+  const classNames = clsx(styles.button, styles[shape]);
+
+  return (
+    <button className={classNames} type={type} {...restProps}>
+      {children}
+    </button>
+  );
+}
