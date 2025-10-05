de57d8d6b64f72d83b638e0211b6a720dcd87f28
Ilya <filil2003@yandex.ru>
2025-10-05 21:59:40 +0300

    feat(settings): add basic user settings with Zustand
    - Implemented minimal Zustand store for user preferences
    - Added persistence to LocalStorage
    - Settings not yet connected to app logic

--- src/pages/HomePage/ui/HomePage.tsx
+++ src/pages/HomePage/ui/HomePage.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
+import { useSettingsStore } from "#entities/settings";
 import {
   CurrentWeather,
   HourlyForecast,
@@ -8,15 +9,18 @@ import {
   weatherQueries,
 } from "#entities/weather";
 import { SearchCity } from "#features/SearchCity";
+import { Settings } from "#features/Settings";
 import styles from "./HomePage.module.css";
 
 export function HomePage() {
   const [city, setCity] = useState("Санкт-Петербург");
+  const preferredLanguage = useSettingsStore((state) => state.language);
 
   const { data } = useQuery(
     weatherQueries.forecast({
       q: city,
       days: 3,
+      lang: preferredLanguage,
     }),
   );
 
@@ -24,6 +28,7 @@ export function HomePage() {
     <>
       <header className={styles.header}>
         <SearchCity city={city} onCityChange={setCity} />
+        <Settings />
       </header>
       <main className={styles.main}>
         {data && (
