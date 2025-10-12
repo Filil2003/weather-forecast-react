cb2cfbc7a10af838a06ce0d4d8cfe3b5fbb78781
Ilya <filil2003@yandex.ru>
2025-10-12 19:33:20 +0300

    refactor(footer): update layout and styles according to mockup

--- src/widgets/Footer/ui/Footer.tsx
+++ src/widgets/Footer/ui/Footer.tsx
@@ -1,13 +1,16 @@
-import { Trans } from "react-i18next";
+import { useTranslation } from "react-i18next";
+import { Text } from "#shared/ui";
 import styles from "./Footer.module.css";
 
 export function Footer() {
+  const { t } = useTranslation();
+
   return (
     <footer className={styles.footer}>
-      <Trans i18nKey="terms.agreement">
-        Weather data provided by{" "}
+      <Text as="p" weight="bold">
+        {t("footer.weather-data-source")}{" "}
         <a href="https://www.weatherapi.com/">WeatherAPI.com</a>
-      </Trans>
+      </Text>
     </footer>
   );
 }
