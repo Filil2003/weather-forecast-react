63d70ca7d95be7c979e585fb32038f5c5ef4892e
Ilya <filil2003@yandex.ru>
2025-10-19 21:18:39 +0300

    refactor(ui): extend Button to support anchor element

--- src/shared/ui/Button/ui/Button.tsx
+++ src/shared/ui/Button/ui/Button.tsx
@@ -2,22 +2,32 @@ import clsx from "clsx";
 import type { ComponentProps } from "react";
 import styles from "./Button.module.css";
 
-interface Props extends ComponentProps<"button"> {
-  shape?: "round" | "normal";
+interface LinkSpecificProps extends ComponentProps<"a"> {
+  type: "link";
 }
 
-export function Button({
-  children,
-  className,
-  shape = "normal",
-  type = "button",
-  ...restProps
-}: Props) {
+type Props = (ComponentProps<"button"> | LinkSpecificProps) & {
+  shape?: "round" | "normal";
+};
+
+function Button(props: Props) {
+  const { shape = "normal", className, children, ...restProps } = props;
   const classNames = clsx(styles.button, styles[shape], className);
 
+  if (restProps.type === "link") {
+    const { type: _, ...linkProps } = restProps;
+    return (
+      <a className={classNames} {...linkProps}>
+        {children}
+      </a>
+    );
+  }
+
   return (
-    <button className={classNames} type={type} {...restProps}>
+    <button className={classNames} type="button" {...restProps}>
       {children}
     </button>
   );
 }
+
+export { Button };
